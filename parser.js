eaa7f9b84ad506c310f3d1c4a84c2073ea1ec681
Richard Mena <richardmf9425@yahoo.com>
2019-09-19 00:44:34 -0400

    add first stage

---
+++ parser.js
@@ -0,0 +1,40 @@
+require('dotenv').config();
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+const nightmare = require('nightmare')();
+
+const args = process.argv.slice(2);
+const url = args[0];
+const minPrice = args[1];
+
+checkPrice();
+
+async function checkPrice() {
+	try {
+		const priceString = await nightmare
+			.goto(url)
+			.wait('#priceblock_ourprice')
+			.evaluate(() => document.getElementById('priceblock_ourprice').innerText)
+			.end();
+		const priceNumber = parseFloat(priceString.replace('$', ''));
+		if (priceNumber < minPrice) {
+			await sendEmail('Price Is Low', `The price on ${url} has dropped below ${minPrice}`);
+		}
+	} catch (e) {
+		await sendEmail('Amazon Price Checker Error', e.message);
+		throw e;
+	}
+}
+
+function sendEmail(subject, body) {
+	const email = {
+		to: 'richardmf9425@yahoo.com',
+		from: 'amazon-tracker@web.com',
+		subject: subject,
+		text: body,
+		html: body
+	};
+
+	return sgMail.send(email);
+}
